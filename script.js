415747b732270ac5d9e84e0656a3e04dcaef9636
Kulagin Gregory <kulagin.gregory@yandex.ru>
2023-08-06 16:52:33 +0400

    Последние доработки

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //Задание 1
 // let a = 10;
-// alert (a);
+// alert(a);
 // a = 20;
 // alert(a);
 
 //Задание 2
-// let releaseOfIphone = 2007;
+// const releaseOfIphone = 2007;
 // alert(releaseOfIphone);
 
 // Задание 3
-// let creatorOfJavaScript ='Вrendan Eyck';
+// const creatorOfJavaScript ='Вrendan Eyck';
 // alert(creatorOfJavaScript);
 
 //Задание 4
